d7ca1151ad03504b8241527822c7777a12728f65
unknown <ante.dujic0506@gmail.com>
2021-02-15 10:52:28 +0000

    Week 02 Task_v1.1

--- bmi.py
+++ bmi.py
@@ -3,13 +3,13 @@
 
 # Inputting height and weight
 
-height = int (input ("Enter height (cm): "))
+height = int (input ("Enter height (cm): "))                # convert input to int, because it's str by default
 weight = int (input ("Enter weight (kg): "))
 
 # Calculating and rounding BMI to 2 decimal places
 
-BMI = round((weight/((height/100)**2)),2)
+BMI = round((weight/((height/100)**2)),2)                   # formula for BMI, rounding to 2 decimals
 
 # Printing the BMI
 
-print ("BMI is:",BMI)
+print ("BMI is:",BMI)                                       # output
