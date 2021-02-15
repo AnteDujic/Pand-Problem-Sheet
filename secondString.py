d7823cbb64c3b4749ee35a95c2e3f63ebcbfd396
unknown <ante.dujic0506@gmail.com>
2021-02-15 10:54:11 +0000

    Week 03 Task_v1.1

--- secondString.py
+++ secondString.py
@@ -3,12 +3,14 @@
 
 # Inputting a string
 
-stringInput = str (input ("Please enter a sentence: "))
+stringInput = input ("Please enter a sentence: ")
 
 # Taking every second character of a string
+# REF: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 stringSecond = (stringInput [1::2])
 
 # Outputting every second character in reverse
+# REF: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 print (stringSecond [::-1])
