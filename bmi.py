1e21b1cfc2c0a2fa5bb551064e3f92754cc03e58
unknown <ante.dujic0506@gmail.com>
2021-03-22 10:29:25 +0000

    Sorting out BMI.py

--- bmi.py
+++ bmi.py
@@ -3,8 +3,8 @@
 
 # Inputting height and weight
 
-height = int (input ("Enter height (cm): "))                # convert input to int, because it's str by default
-weight = int (input ("Enter weight (kg): "))
+height = float (input ("Enter height (cm): "))                # convert input to float, because it's str by default
+weight = float (input ("Enter weight (kg): "))
 
 # Calculating and rounding BMI to 2 decimal places
 
