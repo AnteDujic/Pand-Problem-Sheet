eb21922ce92ac07d31cebde483658c23891a0e0f
unknown <ante.dujic0506@gmail.com>
2021-03-24 14:19:02 +0000

    Tyding up

--- es.py
+++ es.py
@@ -3,14 +3,16 @@
 # Author: Ante Dujic
 import sys
 
-myFile = sys.argv[1]
-stringToCount = input ("Please write a string you want to count:")
+myFile = sys.argv[1]                                                        # setting file to open to be taken from command line
+stringToCount = input ("Please write a string you want to count:")          # promting user to input a string to be count
 
-with open (myFile, "rt") as f:
+with open (myFile, "rt") as f:                                              # raeding the file (rt - read text file)
     data = f.read ()
-    stringCount = data.count (stringToCount)
-    print (stringCount)
+    stringCount = data.count (stringToCount)                                # counting inputed string
+    print (stringCount)                                                     # output the counted string
 
     # https://www.w3schools.com/python/ref_string_count.asp
     # https://www.guru99.com/python-string-count.html#:~:text=Python%20count,want%20the%20search%20to%20begin.
     # https://realpython.com/python-command-line-arguments/
+    # https://www.tutorialsteacher.com/python/sys-module
+    # https://docs.python.org/3/library/sys.html
